8c04532703b7ee8acb5acf6076285c56084b1631
David <wjw3298@gmail.com>
2024-06-25 14:33:19 -0400

    added server

--- client/src/pages/upload.jsx
+++ client/src/pages/upload.jsx
@@ -2,10 +2,42 @@ import React from 'react'
 import Navbar from '../components/navbar'
 
 function upload() {
+
+    const handleFileSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+
+        fetch('ypload_file', {
+            method: 'POST',
+            body: formData,
+        })
+        .then((res) => res.json())
+        .then((data)=> {
+            console.log(data);
+        })
+        .catch((error)=> {
+            console.error('Error uploading file:', error);
+        })
+    }
+
   return (
     <div>
         <Navbar/>
-        <h1>Upload</h1>
+        <div>
+            <form encType='multipart/form-data' onSubmit={handleFileSubmit}>
+                <div className='mb-3'>
+                    <label htmlFor='formFile' className='form-label'>
+                        Upload Your File
+                    </label>
+                    <input name='file' className='form-control' type='file' id='formFile'/>
+                </div>
+                <div className='form-group'>
+                    <button className='btn btn-primary' type='submit'>
+                        Submit
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
   )
 }
