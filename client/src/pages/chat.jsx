e3b6711b8995aa2e7e6aea705f72f48767392611
David <wjw3298@gmail.com>
2024-06-25 12:23:27 -0400

    navbar setup

---
+++ client/src/pages/chat.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/navbar'
+
+function chat() {
+  return (
+    <div>
+        <Navbar />
+        <h1>Chat</h1>
+    </div>
+  )
+}
+
+export default chat
